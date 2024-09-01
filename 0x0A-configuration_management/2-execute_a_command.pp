db7acf9d610accd5d54482d16993af4ee40e4c0e
Under20techie <under20techie@gmail.com>
2024-09-01 21:12:21 +0100

    you

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 
-exec {'pkill':
+exec {'killmenow':
   command => 'pkill -f killmenow',
   cwd     => '/root/maryagiamah/alx-system_engineering-devops/0x0A-configuration_management',
   path    => '/usr/bin/',
