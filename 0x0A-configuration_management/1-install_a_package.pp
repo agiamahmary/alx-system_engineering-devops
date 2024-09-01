d9ef1cf24f4b472a5804601c1c8ab83e0ab2d7ce
Under20techie <under20techie@gmail.com>
2024-09-01 20:41:01 +0100

    you

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,15 @@ package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
+}
+
+package { 'Python':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+  require  => Package['Flask']
+}
